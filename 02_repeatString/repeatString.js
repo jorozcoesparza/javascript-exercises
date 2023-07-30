aab5f3f0efeea005d7088474f545b0f4d011e3d6
jorozcoesparza <jorozco842@gmail.com>
2023-07-30 15:39:32 -0500

    Solves first part of exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, number) {
+let finalString = "";
+    while (number > 0) {
+        finalString += string
+        number--
+    }
+    return finalString;
 };
 
 // Do not edit below this line
